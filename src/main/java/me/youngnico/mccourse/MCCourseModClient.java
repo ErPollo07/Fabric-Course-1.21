04d9a74e3da296a9ce55567e764ee0881d9f7f53
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-08-03 15:29:50 +0200

    Init commit

---
+++ src/main/java/me/youngnico/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package me.youngnico.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
